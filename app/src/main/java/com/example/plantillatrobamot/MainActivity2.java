e543eda965850568f43a77fda28bba6b75b19d3d
RubiBalles <104781672+RubiBalles@users.noreply.github.com>
2023-05-31 11:11:20 +0200

    json,html

--- app/src/main/java/com/example/plantillatrobamot/MainActivity2.java
+++ app/src/main/java/com/example/plantillatrobamot/MainActivity2.java
@@ -8,22 +8,42 @@ import android.os.Bundle;
 import android.text.Html;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
 public class MainActivity2 extends AppCompatActivity {
     private TextView t;
+    private TextView palsol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         t = findViewById(R.id.textView);
-
+        palsol=findViewById(R.id.palsol);
         Intent intent = getIntent() ;
         String messageRestriccions = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         String messageParaulesPossibles=intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
+        Thread thread = new Thread (new Runnable () {
+     @Override
+     public void run () {
+                 try {
+                     JSONObject jObject = new JSONObject (agafaHTML(paraulasolucio));
+                     String def = jObject . getString ("d") ;
+                     TextView definicio=findViewById(R.id.definicio);
+                     if (def.isEmpty()){
+                        definicio.setText("Definició no trobada");
+                     }else {
+                         definicio.setText(Html.fromHtml(def, Html.FROM_HTML_MODE_LEGACY));
+                     }
+                     } catch ( Exception e ) {
+                     e . printStackTrace () ;
+                     }
+                 }
+        }) ;
 
         t.setText(messageRestriccions+"\n\n"+messageParaulesPossibles);
     }
@@ -37,9 +57,10 @@ public class MainActivity2 extends AppCompatActivity {
             StringBuffer buffer = new StringBuffer();
             while ((linea = in.readLine()) != null) {
                 buffer.append(linea);
-                buffer.append("\n");  // Agregar un salto de línea si lo deseas
             }
+            in.close();
             linea= buffer.toString();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
