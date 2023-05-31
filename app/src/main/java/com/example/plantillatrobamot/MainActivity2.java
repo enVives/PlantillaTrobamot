f1d744b7191ea474d45b77f1cd17f478e988f091
RubiBalles <104781672+RubiBalles@users.noreply.github.com>
2023-05-31 11:17:06 +0200

    pt2

--- app/src/main/java/com/example/plantillatrobamot/MainActivity2.java
+++ app/src/main/java/com/example/plantillatrobamot/MainActivity2.java
@@ -16,28 +16,27 @@ import java.net.URL;
 
 public class MainActivity2 extends AppCompatActivity {
     private TextView t;
-    private TextView palsol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         t = findViewById(R.id.textView);
-        palsol=findViewById(R.id.palsol);
         Intent intent = getIntent() ;
         String messageRestriccions = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         String messageParaulesPossibles=intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
+        String paraulasolucio=intent.getStringExtra(MainActivity.EXTRA_MESSAGE3);
         Thread thread = new Thread (new Runnable () {
      @Override
      public void run () {
                  try {
                      JSONObject jObject = new JSONObject (agafaHTML(paraulasolucio));
                      String def = jObject . getString ("d") ;
-                     TextView definicio=findViewById(R.id.definicio);
+                    // TextView definicio=findViewById(R.id.definicio);
                      if (def.isEmpty()){
-                        definicio.setText("Definició no trobada");
+                      //  definicio.setText("Definició no trobada");
                      }else {
-                         definicio.setText(Html.fromHtml(def, Html.FROM_HTML_MODE_LEGACY));
+                      //   definicio.setText(Html.fromHtml(def, Html.FROM_HTML_MODE_LEGACY));
                      }
                      } catch ( Exception e ) {
                      e . printStackTrace () ;
